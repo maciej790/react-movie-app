735d18ab44b283d2837b81aa4ba78e91ba390819
maciej790 <maciejmw126@gmail.com>
2021-07-09 17:45:56 +0200

    add animation go top

--- src/style/FirstView.js
+++ src/style/FirstView.js
@@ -4,5 +4,6 @@ export const FirstView = styled.section`
     display: flex;
     flex-direction: column;
     align-items: center;
+    transition: transform 0.5s ease-in-out;
     transform: ${({isTop}) => isTop ? 'translateY(-450px)' : null};
 `;
